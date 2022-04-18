3b68d133742230876d83f89e58686b39ce3d013a
ronLabs <ron.altamir@outlook.com>
2022-04-18 21:01:26 +0000

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
 # puppet manifest to install puppt-lint package.
 
-exec { 'apt-get update':
-  command   => '/usr/bin/apt-get update'
-}
-
 package { 'puppet-lint':
   ensure   => '2.5.0',
   provider => 'gem',
