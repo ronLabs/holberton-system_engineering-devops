be51dd980ce450ab6b470c51ebffa5d8f337b4b2
ronLabs <ron.altamir@outlook.com>
2022-04-19 19:31:28 +0000

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -9,7 +9,7 @@ file {'Configuration File':
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => '  IdentityFile ~/.ssh/holberton'
+  line   => '  IdentityFile ~/.ssh/school'
 }
 
 file_line { 'Turn off passwd auth':
@@ -17,4 +17,3 @@ file_line { 'Turn off passwd auth':
   path   => '/etc/ssh/ssh_config',
   line   => '  PasswordAuthentication no'
 }
-
