0eda106c99963f38215921ab1a8ffb091bced5a9
ronLabs <ron.altamir@outlook.com>
2022-04-19 19:29:16 +0000

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,19 @@
+# This manifests adds 2 lines to ssh_config file to configure a private key and deny password auth 
+
+file {'Configuration File':
+  ensure => 'present',
+  name   => 'ssh_config',
+  path   => '/etc/ssh/ssh_config'
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '  IdentityFile ~/.ssh/holberton'
+}
+
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '  PasswordAuthentication no'
+}
