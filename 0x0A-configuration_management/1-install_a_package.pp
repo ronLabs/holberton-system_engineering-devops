f36d705a71561f897e68beb7f640b640e9504313
ronLabs <ron.altamir@outlook.com>
2022-04-20 18:36:54 +0000

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Using Puppet, install Flask from pip3.
 
 exec { 'flask_install':
-  command  => 'pip3 install Flask==2.1.0',
-  path => '/usr/bin',
+  command => 'pip3 install Flask==2.1.0',
+  path    => '/usr/bin',
 }
