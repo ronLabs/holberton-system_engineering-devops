5b56744581f2330109a0541a91a95fe39c653943
ronLabs <ron.altamir@outlook.com>
2022-04-20 16:13:37 +0000

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # puppet manifest to install a package
 
-exec { 'flask_install':
-  command  => 'pip3 install Flask==2.1.0',
-  provider => 'shell',
+exec { 'install_flask':
+  command  => 'pip3 install flask==2.1.0',
+  provider => shell,
 }
