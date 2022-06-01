aa752224b08fffa35170b0f4c03eda141ea6e639
ronLabs <ron.altamir@outlook.com>
2022-06-01 23:36:45 +0000

    Number of subscribers with Reddit API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,6 @@ url = 'https://www.reddit.com/r/{}/about.json'
 def number_of_subscribers(subreddit):
     """This function fetches reddit and returns the number of subscribers"""
     agent = {'User-Agent': 'My User Agent 1.0'}
-    result = req.get(url.format(subreddit), headers = agent, allow_redirects=False)
+    result = req.get(url.format(subreddit), headers=agent, allow_redirects=False)
     body = result.json().get('data', 0)
     return body.get('subscribers', 0)
