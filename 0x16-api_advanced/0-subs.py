bfef31a901be6a90ab33f925d0913b85c98ef878
ronLabs <ron.altamir@outlook.com>
2022-06-01 23:25:42 +0000

    Number of subscribers with Reddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module makes a request to reddit api"""
+import requests as req
+
+url = 'https://www.reddit.com/r/{}/about.json'
+
+
+def number_of_subscribers(subreddit):
+    """This function fetches reddit and returns the number of subscribers"""
+    agent = {'User-Agent': 'My User Agent 1.0'}
+    result = req.get(url.format(subreddit), headers = agent, allow_redirects=False)
+    body = result.json().get('data', 0)
+    return body.get('subscribers', 0)
